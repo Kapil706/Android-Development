cb6cf2d9b05b76d9d6fe1ffc07a30a78b2798b26
kapil706 <kapilchaudhary706@gmail.com>
2018-09-16 18:42:00 +0530

    view binding

---
+++ ViewBinding/app/src/main/java/com/example/kapil/viewbinding/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kapil.viewbinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+     public static final String TAG= "BTN";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v){
+        Log.d(TAG,"button clicked");
+    }
+}
